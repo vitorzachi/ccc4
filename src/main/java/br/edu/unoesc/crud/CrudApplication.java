f6fb797497b60976774c43b6a7e11ed80ebcb13d
Vitor Zachi Junior <vitorzachi@gmail.com>
2019-08-25 13:48:02 -0300

    init

---
+++ src/main/java/br/edu/unoesc/crud/CrudApplication.java
@@ -0,0 +1,13 @@
+package br.edu.unoesc.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApplication.class, args);
+	}
+
+}
